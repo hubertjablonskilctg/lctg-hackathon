c51ba3bc7244acc4148acefd36de956bcd1c7124
Tomasz Pamula <example@example.com>
2016-07-06 23:00:07 +0200

    Display index.html in main directory.

--- backend/hackaton-engine/hackaton-engine/App_Start/RouteConfig.cs
+++ backend/hackaton-engine/hackaton-engine/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace hackaton_engine
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.IgnoreRoute("");
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
