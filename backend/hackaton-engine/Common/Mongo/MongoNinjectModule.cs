e6cefe2c778d35ad51195a78115873e62910cdbb
Tomasz Sulkowski <TomaszSulkowski@LowCostBeds.Com>
2016-07-07 16:16:24 +0100

    reverting mongo connection string in DEBUG

--- backend/hackaton-engine/Common/Mongo/MongoNinjectModule.cs
+++ backend/hackaton-engine/Common/Mongo/MongoNinjectModule.cs
@@ -11,12 +11,8 @@ namespace Common.Mongo
         {
             Bind<IMongoClient>()
                 .To<MongoClient>().InSingletonScope()
-#if DEBUG
-                .WithConstructorArgument(typeof(string), "mongodb://localhost:27017");
-#endif
-#if RELEASE
+                //.WithConstructorArgument(typeof(string), "mongodb://localhost:27017");
                 .WithConstructorArgument(typeof(string), "mongodb://root:bitnami@13.79.175.46/admin");
-#endif
             Bind<IMongoDatabase>().ToMethod((context) => context.Kernel.Get<IMongoClient>().GetDatabase("core", null));
 
             Bind(typeof(IMongoRepository<>)).To(typeof(MongoRepository<>));
