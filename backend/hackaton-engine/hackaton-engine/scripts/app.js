fb7aae49ec06f6221ba9e3f1efb938b2c6cb65e5
Hubert Jabłoński <hubertjablonski@windowslive.com>
2016-07-07 17:40:56 +0200

    Use Angular for index.html

--- backend/hackaton-engine/hackaton-engine/scripts/app.js
+++ backend/hackaton-engine/hackaton-engine/scripts/app.js
@@ -1,33 +1,23 @@
-$(document).ready(function () {
-    $.ajax({
-        url: "http://takeoff2016-krkteam.azurewebsites.net/api/Test/1"
-    }).then(function (data) {
-        console.log(data.Id + " " + data.Name);
-    });
+angular.module('groupTripApp', [])
+  .controller('MainController', function () {
+      var ctrl = this;
+      
+      ctrl.tripMembers = [];
 
-    var tripMembers = [];
+      ctrl.newMemberName;
 
-    $('#addMember').on('click',
-        function () {
-            var newMemberName = $('#memberEmail').val();
-            if (newMemberName.length) {
-                $("<div class='row member'><div class='col-sm-10'>" + newMemberName + "</div><div class='col-sm-2'><i id='" + newMemberName + "' class='fa fa-close deleteMember'></i></div></div>")
-                    .insertBefore($("#addMemberRow"));
-                $('#memberEmail').val('');
-                tripMembers.push(newMemberName);
-            }
-        });
+      ctrl.addMember = function () {
+          ctrl.tripMembers.push(ctrl.newMemberName);
+          ctrl.newMemberName = "";
+      };
 
-    $('.members-content').on('click', '.fa-close', function () {
-        var memberName = $(this).attr('id');
-        var index = tripMembers.indexOf(memberName);
-        tripMembers.splice(index, 1);;
-        $(this).parent().parent().remove();
-    });
+      ctrl.removeMember = function (member) {
+          var index = ctrl.tripMembers.indexOf(member);
+          ctrl.tripMembers.splice(index, 1);
+      };
 
-    $('#createTrip').on('click',
-    function () {
-        alert("Create trip " + tripMembers[0]);
-    });
-
-});
+      ctrl.createTrip = function () {
+          alert("Create trip " + ctrl.tripMembers[0]);
+      };
+      
+  });
