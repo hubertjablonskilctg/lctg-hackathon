8531dac93c5a779158da6951ec41e656756b1384
Hubert Jabłoński <hubertjablonski@windowslive.com>
2016-07-07 16:11:10 +0200

    Add create trip page

--- backend/hackaton-engine/hackaton-engine/scripts/app.js
+++ backend/hackaton-engine/hackaton-engine/scripts/app.js
@@ -5,8 +5,29 @@ $(document).ready(function () {
         console.log(data.Id + " " + data.Name);
     });
 
-    $('#addMember').on('click', function () {
-        alert("Add member " + $('#memberEmail').val());
+    var tripMembers = [];
+
+    $('#addMember').on('click',
+        function () {
+            var newMemberName = $('#memberEmail').val();
+            if (newMemberName.length) {
+                $("<div class='row member'><div class='col-sm-10'>" + newMemberName + "</div><div class='col-sm-2'><i id='" + newMemberName + "' class='fa fa-close deleteMember'></i></div></div>")
+                    .insertBefore($("#addMemberRow"));
+                $('#memberEmail').val('');
+                tripMembers.push(newMemberName);
+            }
+        });
+
+    $('.members-content').on('click', '.fa-close', function () {
+        var memberName = $(this).attr('id');
+        var index = tripMembers.indexOf(memberName);
+        tripMembers.splice(index, 1);;
+        $(this).parent().parent().remove();
+    });
+
+    $('#createTrip').on('click',
+    function () {
+        alert("Create trip " + tripMembers[0]);
     });
 
 });
