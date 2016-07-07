539f8ec7e6ab6058c66d530930f783338cd72bdd
elsendar <elsendar@hotmail.com>
2016-07-07 17:27:50 +0100

    Change seeding values

--- backend/hackaton-engine/hackaton-engine/Controllers/DbSeedController.cs
+++ backend/hackaton-engine/hackaton-engine/Controllers/DbSeedController.cs
@@ -40,16 +40,16 @@ namespace hackaton_engine.Controllers
 
             _hotelRepository.Remove(x => true);
 
-            for (int i = 1; i <= 22; ++i)
+            for (int i = 1; i <= 100; ++i)
             {
                 var hotel = new Hotel();
                 hotel.Id = i;
-                hotel.Price = 100 + i * random.Next(900);
+                hotel.Price = 100 + random.Next(900);
                 hotel.Name = string.Format("hotel{0}", i);
                 hotel.Address = "";
                 hotel.Localization = (Localizations)values.GetValue(random.Next(values.Length));
                 hotel.Address = String.Format("{0} Street, {1}", i, hotel.Localization.ToString());
-                hotel.Tags = new[] { (Tags)(random.Next(5)), (Tags)(random.Next(5)) };
+                hotel.Tags = new[] { (Tags)(i % 6), (Tags)((i + 1) % 6), (Tags)((i + 4) % 6), (Tags)((i + 7) % 6) };
                 hotel.MustHaves = new[] { (MustHaves)(i % 2) };
                 hotel.ImageUrl = hotel.Name;
 
@@ -74,6 +74,8 @@ namespace hackaton_engine.Controllers
         {
             _groupRepository.Remove(x => true);
 
+            Random random = new Random();
+
             for (int i = 1; i <= 10; ++i)
             {
                 var group = new Group();
@@ -83,12 +85,33 @@ namespace hackaton_engine.Controllers
                 group.UserHotelUpVotes = new Dictionary<int, int[]>();
                 group.UserHotelUpVotes.Add(i, new[] { i, i + 1 });
                 group.UserHotelUpVotes.Add(i + 1, new[] { i, i + 1, i + 2 });
-                group.UserHotelUpVotes.Add(i + 2, new[] { i, i + 3 });
+                //group.UserHotelUpVotes.Add(i + 2, new[] { i, i + 3 });
 
                 group.UserPreferences = new Dictionary<int, Preference>();
-                group.UserPreferences.Add(i, new Preference());
-                group.UserPreferences.Add(i + 1, new Preference());
-                group.UserPreferences.Add(i + 2, new Preference());
+                group.UserPreferences.Add(i, new Preference()
+                {
+                    DateRange = new Tuple<DateTime, DateTime>(DateTime.MinValue, DateTime.MaxValue),
+                    PriceRange = new Tuple<double, double>(random.Next(100), 300 + random.Next(1700)),
+                    Localizations = new[] { (Localizations)(i % 7), (Localizations)((i + 1) % 7), (Localizations)((i + 2) % 7), (Localizations)((i + 6) % 7) },
+                    Tags = new[] { (Tags)(i % 6), (Tags)(i + 1 % 6), (Tags)(i + 2 % 6) },
+                    MustHaves = new MustHaves[0]
+                });
+                group.UserPreferences.Add(i + 1, new Preference()
+                {
+                    DateRange = new Tuple<DateTime, DateTime>(DateTime.MinValue, DateTime.MaxValue),
+                    PriceRange = new Tuple<double, double>(random.Next(100), 700 + random.Next(500)),
+                    Localizations = new[] { (Localizations)(i % 7), (Localizations)((i + 2) % 7), (Localizations)((i + 3) % 7) },
+                    Tags = new[] { (Tags)(i % 6), (Tags)((i + 2) % 6), (Tags)(i + 4 % 6) },
+                    MustHaves = new[] { (MustHaves)random.Next(2) }
+                });
+                //group.UserPreferences.Add(i + 2, new Preference()
+                //{
+                //    DateRange = new Tuple<DateTime, DateTime>(DateTime.MinValue, DateTime.MaxValue),
+                //    PriceRange = new Tuple<double, double>(200, 200 + random.Next(2000)),
+                //    Localizations = new[] { (Localizations)random.Next(7), (Localizations)random.Next(7) },
+                //    Tags = new[] { (Tags)random.Next(6), (Tags)random.Next(6) },
+                //    MustHaves = new[] { (MustHaves)random.Next(2) }
+                //});
 
                 _groupRepository.Add(group);
             }
