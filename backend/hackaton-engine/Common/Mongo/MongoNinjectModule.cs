21c141301fe5fe9e4922a2e3edc31003613ed715
Tomasz Sulkowski <TomaszSulkowski@LowCostBeds.Com>
2016-07-07 09:15:32 +0100

    mongo hackathon connection string fix

--- backend/hackaton-engine/Common/Mongo/MongoNinjectModule.cs
+++ backend/hackaton-engine/Common/Mongo/MongoNinjectModule.cs
@@ -12,14 +12,16 @@ namespace Common.Mongo
             var credential = MongoCredential.CreateMongoCRCredential("admin", "root", "bitnami");
             var mongoSettings = new MongoClientSettings
             {
-                Credentials = new[] { credential }
+                Credentials = new[] { credential },
+                
             };
 
 
             Bind<IMongoClient>()
                 .To<MongoClient>().InSingletonScope()
                 //.WithConstructorArgument(typeof(string), "mongodb://localhost:27017");
-                .WithConstructorArgument(typeof(MongoClientSettings), mongoSettings);
+                .WithConstructorArgument(typeof(string), "mongodb://root:bitnami@13.79.175.46/admin");
+                //.WithConstructorArgument(typeof(MongoClientSettings), mongoSettings);
             Bind<IMongoDatabase>().ToMethod((context) => context.Kernel.Get<IMongoClient>().GetDatabase("core", null));
 
             Bind(typeof(IMongoRepository<>)).To(typeof(MongoRepository<>));
